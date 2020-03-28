370af8233a47ff301090db87c46ee7053f484af1
Löhmann, Dennis <dennis.loehmann@lhind.dlh.de>
2020-03-28 22:43:41 +0100

    change docker user name

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,17 @@
 
 
 cd $TRAVIS_BUILD_DIR/contacts-backend
-docker build -t raghav141988/contacts-backend:latest -t raghav141988/contacts-backend:$BUILD_ID -f ./Dockerfile .
+docker build -t deloehmann/contacts-backend:latest -t deloehmann/contacts-backend:$BUILD_ID -f ./Dockerfile .
 cd $TRAVIS_BUILD_DIR
 ls -l
 echo $PWD
-docker build -t raghav141988/contacts-frontend:latest -t raghav141988/contacts-frontend:$BUILD_ID -f ./contacts-frontend/Dockerfile ./contacts-frontend
-docker push raghav141988/contacts-backend:latest
-docker push raghav141988/contacts-frontend:latest
+docker build -t deloehmann/contacts-frontend:latest -t deloehmann/contacts-frontend:$BUILD_ID -f ./contacts-frontend/Dockerfile ./contacts-frontend
+docker push deloehmann/contacts-backend:latest
+docker push deloehmann/contacts-frontend:latest
 
-docker push raghav141988/contacts-backend:$BUILD_ID
-docker push raghav141988/contacts-frontend:$BUILD_ID
+docker push deloehmann/contacts-backend:$BUILD_ID
+docker push deloehmann/contacts-frontend:$BUILD_ID
 
 kubectl apply -f ./contacts-deployment/k8s
-kubectl set image deployments/contact-frontend-deployment contacts-frontend=raghav141988/contacts-frontend:$BUILD_ID
-kubectl set image deployments/contact-backend-deployment contacts-api=raghav141988/contacts-backend:$BUILD_ID
+kubectl set image deployments/contact-frontend-deployment contacts-frontend=deloehmann/contacts-frontend:$BUILD_ID
+kubectl set image deployments/contact-backend-deployment contacts-api=deloehmann/contacts-backend:$BUILD_ID
